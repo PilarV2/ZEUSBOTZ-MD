f67de7a1898480123694e97b59631ae15633071f
AKU SAYANG PACAR MU <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-06-05 15:45:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ let wktus = moment.tz('Asia/Jakarta').format('ss')
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
-global.owner = ['6289625556161','62831433937633', '62896255561617'] // Put your number here
+global.owner = ['6285706735450','6285706735450', '6285706735450'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -65,19 +65,19 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'ZEUSBOTZ-MD'
-global.author = `By Pilar`
-global.wm = 'ZEUS BOT - MD' 
+global.packname = 'ARACHUU-MD'
+global.author = `By VERDIKAWAII`
+global.wm = 'ARACHUU - SANGEAN' 
 global.watermark = wm
 global.instagram = 'https://instagram.com/not_pilar32'
 global.botdate = `❏ ${wktuh} H  ${wktum} M  ${wktus} S\n❏ ${week} ${date}`
-global.media = 'https://telegra.ph/file/ccb2359d3925fd7af58cd.jpg'
-global.github = 'https://github.com/PilarV2'
+global.media = 'https://telegra.ph/file/7778a666f59c9f2d3554a.png'
+global.github = 'https://github.com/VERDIBOTZ1'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*L O A D I N G*_'
-global.eror = '_*Server Error*_'
-global.benar = 'Benar ✅\n'
-global.salah = 'Salah ❌\n'
+global.eror = '_*Server Error kebayakan mikir*_'
+global.benar = 'Benar pinter tayang ✅\n'
+global.salah = 'Salah tayang lol❌\n'
 
 global.multiplier = 69 // The higher, The harder levelup
 
