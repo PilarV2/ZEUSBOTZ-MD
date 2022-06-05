4e5bb691ffb08cba95d4f949ca196f2b183917a0
AKU SAYANG PACAR MU <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-06-05 15:51:05 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,14 +1,15 @@
 function handler(m) {
 const PhoneNumber = require('awesome-phonenumber')
-let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "6289523258649-1604595598@g.us"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'Centauri', sellerJid: '0@s.whatsapp.net'}}}                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "6289523258649-1604595598@g.us"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'Centauri', sellerJid: '0@s.whatsapp.net'}}}
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
                     + 'N:; Pilar;;;'
                     + 'FN: PilarXcode\n' // full name
                     + 'ORG:Pilar;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6289625556161:+628-962-555-6161\n' // WhatsApp ID + phone number
+                    + 'TEL;type=CELL;type=VOICE;waid=6285706735450:+628-570-673-5450\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'PilarXcode', contacts: [{ vcard }] } }, { quoted: ftroli })
-m.reply(m.chat,'*Tuh nomor owner ku*\n*Chat jika PENTING*', m)
+m.reply(m.chat,'*Tuh nomor owner ku sangean*\n*Chat jika PENTING*', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
